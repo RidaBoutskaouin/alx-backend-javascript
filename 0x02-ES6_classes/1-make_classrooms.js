8f4306fdc401f0e93fe1f6f6635b1a5b2be0adbe
Rida Boutskaouin <rida.boutskaouin@edu.uiz.ac.ma>
2024-04-12 00:45:17 +0000

    new Class(1)

---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,11 @@
+/* eslint-disable */
+import Classroom from "./0-classroom.js";
+
+export default function initializeRooms() { 
+	const rooms = [
+		new Classroom(19),
+		new Classroom(20),
+		new Classroom(34)
+	];
+	return rooms;
+}
